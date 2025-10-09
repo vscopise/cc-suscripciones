5f471ee3a91708af96ac537a743c57bb745207f1
Vicente Scópise <vscopise@gmail.com>
2025-10-09 14:00:05 -0300

    9/10 - 9

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -125,7 +125,7 @@ export async function POST(req: Request) {
 
         //prisma.createMany
       }
-      let createMany = prisma.client.createMany({
+      let createMany = await prisma.client.createMany({
         data,
         skipDuplicates: true,
       });
