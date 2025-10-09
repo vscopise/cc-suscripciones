ba5a8f4e6b0fafc7c1a7759c138a90ce7139b98c
Vicente Scópise <vscopise@gmail.com>
2025-10-09 10:02:04 -0300

    9/10 - 5

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -1,116 +1,153 @@
-import { auth } from '@/auth.config';
-import prisma from '@/lib/prisma';
-import { revalidatePath } from 'next/cache';
-import { NextResponse, NextRequest } from 'next/server'
-import { number, z } from 'zod';
+import { auth } from "@/auth.config";
+import prisma from "@/lib/prisma";
+import { revalidatePath } from "next/cache";
+import { NextResponse, NextRequest } from "next/server";
+import { number, z } from "zod";
 
 export async function POST(req: Request) {
+  interface SchemaInterfce {
+    [key: string]: string[];
+  }
 
-    interface SchemaInterfce {
-        [key: string]: string[]
+  const session = await auth();
+
+  var userId = session!.user.id;
+
+  const body = await req.json();
+
+  const csv = body.csv.data;
+
+  //Error si no vienen datos
+  if (csv.length <= 1) {
+    return NextResponse.json("No hay datos suficientes", { status: 201 });
+  }
+
+  const out: any = [];
+  const propsRow = csv[0];
+  csv.forEach((row: string[], i: number) => {
+    if (i === 0) {
+      return;
     }
+    const addMe: any = {};
+    row.forEach((datum, j) => (addMe[propsRow[j]] = datum));
+    out.push(addMe);
+  });
 
-    const session = await auth();
+  //Validación de datos
+  const schema: any = {
+    Client: z.array(
+      z.object({
+        id: z.string().uuid().optional().nullable(),
+        Nombre: z.string().optional(),
+        Apellido: z.string().optional(),
+        "Tipo Documento": z.string().optional(),
+        "Número Documento": z.string().optional(),
+        "Número de Contacto": z.string().optional(),
+        "Correo Electrónico": z.string().optional(),
+        Dirección: z.string().optional(),
+        Ciudad: z.string().optional(),
+        Estado: z.string().optional(),
+        País: z.string().optional(),
+        "Usuario asignado": z.string().optional(),
+      })
+    ),
+    Card: z.array(
+      z.object({
+        "Correo Electrónico": z.string().email(),
+        Número: z.string(),
+        //'Vencimiento': z.string().regex(new RegExp(/^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0,1,2])\/(19|20)\d{2}$/)).nullish(),
+        Vencimiento: z.string().optional(),
+        //'CVV': z.string().regex(new RegExp(/^\d{3,4}$/)).nullish(),
+        CVV: z.string().optional(),
+      })
+    ),
+    Subscription: z.array(
+      z.object({
+        "email cliente": z.string().email(),
+        inicio: z.string().date().nullish().or(z.string().max(0)),
+        "último pago": z.string().date().nullish().or(z.string().max(0)),
+        plan: z.string().optional(),
+        monto: z.string().optional(),
+        "método de pago": z.string().optional(),
+        "período de pago": z.string().optional(),
+        repartidor: z.string().optional(),
+        comentarios: z.string().optional(),
+      })
+    ),
+  };
 
-    var userId = session!.user.id;
+  const key: string = body.item;
 
-    const body = await req.json();
+  const result = schema[key].safeParse(out);
 
-    const csv = body.csv.data;
+  if (result.success === false)
+    return NextResponse.json("Formato de datos incorrecto", { status: 201 });
 
-    //Error si no vienen datos
-    if (csv.length <= 1) {
-        return NextResponse.json('No hay datos suficientes', { status: 201 });
-    }
+  var i = 0;
 
-    const out: any = [];
-    const propsRow = csv[0];
-    csv.forEach((row: string[], i: number) => {
-        if (i === 0) { return; }
-        const addMe: any = {};
-        row.forEach((datum, j) => addMe[propsRow[j]] = datum);
-        out.push(addMe);
-    });
+  if (key === "Client") {
+    while (i < result.data.length) {
+      var row = result.data[i];
+      if ("" !== row["Usuario asignado"]) {
+        var usuarioAsignado = await prisma.user.findFirst({
+          where: {
+            email: String(row["Usuario asignado"]),
+          },
+        });
 
-    //Validación de datos
-    const schema: any = {
-        Client: z.array(z.object({
-            'id': z.string().uuid().optional().nullable(),
-            'Nombre': z.string().optional(),
-            'Apellido': z.string().optional(),
-            'Tipo Documento': z.string().optional(),
-            'Número Documento': z.string().optional(),
-            'Número de Contacto': z.string().optional(),
-            'Correo Electrónico': z.string().optional(),
-            'Dirección': z.string().optional(),
-            'Ciudad': z.string().optional(),
-            'Estado': z.string().optional(),
-            'País': z.string().optional(),
-            'Usuario asignado': z.string().optional(),
-        })),
-        Card: z.array(z.object({
-            'Correo Electrónico': z.string().email(),
-            'Número': z.string(),
-            //'Vencimiento': z.string().regex(new RegExp(/^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0,1,2])\/(19|20)\d{2}$/)).nullish(),
-            'Vencimiento': z.string().optional(),
-            //'CVV': z.string().regex(new RegExp(/^\d{3,4}$/)).nullish(),
-            'CVV': z.string().optional(),
-        })),
-        Subscription: z.array(z.object({
-            'email cliente': z.string().email(),
-            'inicio': z.string().date().nullish().or(z.string().max(0)),
-            'último pago': z.string().date().nullish().or(z.string().max(0)),
-            'plan': z.string().optional(),
-            'monto': z.string().optional(),
-            'método de pago': z.string().optional(),
-            'período de pago': z.string().optional(),
-            'repartidor': z.string().optional(),
-            'comentarios': z.string().optional(),
-        })),
-    }
+        if (usuarioAsignado) {
+          userId = usuarioAsignado.id;
+        } else {
+          userId;
+        }
+      }
+      const { id } = row;
+      var countryId = "UY";
+      var country = await prisma.country.findFirst({
+        where: { name: row["País"] },
+      });
+      if (country) {
+        countryId = country.id;
+      }
+      var identification =
+        row["Tipo Documento"] == "" ? 0 : +row["Tipo Documento"];
 
-    const key: string = body.item;
+      if (id) {
+        //Actualizar
+        client = await prisma.client.update({
+          where: { id },
+          data: {
+            name: row["Nombre"],
+            lastName: row["Apellido"],
+            phone: row["Número de Contacto"],
+            email: row["Correo Electrónico"],
+            address: row["Dirección"],
+            city: row["Ciudad"],
+            state: row["Estado"],
+            identification,
+            countryId,
+            userId,
+          },
+        });
+      } else {
+        //Crear
+        client = await prisma.client.create({
+          data: {
+            name: row["Nombre"],
+            lastName: row["Apellido"],
+            phone: row["Número de Contacto"],
+            email: row["Correo Electrónico"],
+            address: row["Dirección"],
+            city: row["Ciudad"],
+            state: row["Estado"],
+            identification,
+            countryId,
+            userId,
+          },
+        });
+      }
 
-    const result = schema[key].safeParse(out);
-
-    if (result.success === false) return NextResponse.json('Formato de datos incorrecto', { status: 201 });
-
-    var i = 0;
-
-    if (key === 'Client') {
-        while (i < result.data.length) {
-            var row = result.data[i];
-            var email = row['Correo Electrónico'];
-            if('' !== row['Usuario asignado']) {
-                var usuarioAsignado = await prisma.user.findFirst({
-                    where: {
-                        email: String(row['Usuario asignado'])
-                    }
-                });
-                if(usuarioAsignado) userId = usuarioAsignado.id;
-
-            }
-            const {id, ...rest} = row;
-
-            if (id) {
-                //Actualizar
-                client = await prisma.client.update({
-                    where: {id},
-                    data: {
-                        ...rest
-                    }
-                })
-
-            } else {
-                //Crear
-                client = await prisma.client.create({
-                    data: {
-                        ...rest
-                    }
-                })
-            }
-
-            /* if ('' == email) {
+      /* if ('' == email) {
                 try {
                     await prisma.client.upsert({
                         where: {
@@ -182,96 +219,99 @@ export async function POST(req: Request) {
                     return NextResponse.json('Error al importar datos', { status: 201 });
                 }
             } */
-            i++;
-        }
-        revalidatePath('/clients/');
-    } else if (key === 'Subscription') {
-        while (i < result.data.length) {
-            var row = result.data[i];
-            var data: any = []
-            var client = await prisma.client.findUnique({
-                where: {
-                    //email: row['email cliente']
-                    id: row['id']
-                }
-            });
+      i++;
+    }
+    revalidatePath("/clients/");
+  } else if (key === "Subscription") {
+    while (i < result.data.length) {
+      var row = result.data[i];
+      var data: any = [];
+      var client = await prisma.client.findUnique({
+        where: {
+          //email: row['email cliente']
+          id: row["id"],
+        },
+      });
 
-            if (!client) return NextResponse.json('Cliente no registrado', { status: 201 });
+      if (!client)
+        return NextResponse.json("Cliente no registrado", { status: 201 });
 
-            var plan = await prisma.plan.findFirst({
-                where: {
-                    name: row['plan']
-                }
-            })
-            if (!plan) return NextResponse.json('Plan no registrado', { status: 201 });
+      var plan = await prisma.plan.findFirst({
+        where: {
+          name: row["plan"],
+        },
+      });
+      if (!plan)
+        return NextResponse.json("Plan no registrado", { status: 201 });
 
-            data.push({
-                dateStart: new Date(row['inicio']),
-                dateLastPay: row['último pago'] ? new Date(row['último pago']) : null,
-                amount: parseInt(row['monto']),
-                paymentMethod: row['método de pago'] !== ''
-                    ? row['método de pago'].replace(/\s+/g, '')
-                    : 'MercadoPago',
-                delivery: row['repartidor'],
-                planId: plan.id,
-                clientId: client.id,
-            });
-            i++;
-        }
-        try {
-            await prisma.subscription.createMany({
-                data,
-                skipDuplicates: true
-            })
-        } catch (error) {
-            return NextResponse.json('Error al importar datos', { status: 201 });
-        }
-
-        revalidatePath('/subscriptions/');
-    } else if (key === 'Card') {
-        var data: any = [];
-        while (i < result.data.length) {
-            var row = result.data[i];
-            var data: any = []
-
-            try {
-                var client = await prisma.client.findUnique({
-                    where: {
-                        //email: row['Correo Electrónico']
-                        id: row['id']
-                    }
-                });
-
-                if (!client) return NextResponse.json('Cliente no registrado', { status: 201 });
-
-                var expiration = row['Vencimiento'].replace(/(\d+[/])(\d+[/])/, '$2$1');
-
-                await prisma.creditCard.upsert({
-                    where: {
-                        number: parseInt(row['Número']),
-                    },
-                    create: {
-                        number: parseInt(row['Número']),
-                        expiration: '' === expiration ? new Date('01/01/1900') : new Date(expiration),
-                        cvv: '' === row['CVV'] ? 999 : parseInt(row['CVV']),
-                        clientId: client.id
-                    },
-                    update: {
-                        expiration: '' === expiration ? new Date('01/01/1900') : new Date(expiration),
-                        cvv: '' === row['CVV'] ? 999 : parseInt(row['CVV']),
-                        clientId: client.id
-                    },
-                })
-
-
-            } catch (error) {
-                console.log(error)
-                return NextResponse.json('Error al importar datos', { status: 201 });
-            }
-            i++;
-        }
+      data.push({
+        dateStart: new Date(row["inicio"]),
+        dateLastPay: row["último pago"] ? new Date(row["último pago"]) : null,
+        amount: parseInt(row["monto"]),
+        paymentMethod:
+          row["método de pago"] !== ""
+            ? row["método de pago"].replace(/\s+/g, "")
+            : "MercadoPago",
+        delivery: row["repartidor"],
+        planId: plan.id,
+        clientId: client.id,
+      });
+      i++;
+    }
+    try {
+      await prisma.subscription.createMany({
+        data,
+        skipDuplicates: true,
+      });
+    } catch (error) {
+      return NextResponse.json("Error al importar datos", { status: 201 });
     }
 
-    return NextResponse.json('Archivo subido correctamente', { status: 200 });
-}
+    revalidatePath("/subscriptions/");
+  } else if (key === "Card") {
+    var data: any = [];
+    while (i < result.data.length) {
+      var row = result.data[i];
+      var data: any = [];
 
+      try {
+        var client = await prisma.client.findUnique({
+          where: {
+            //email: row['Correo Electrónico']
+            id: row["id"],
+          },
+        });
+
+        if (!client)
+          return NextResponse.json("Cliente no registrado", { status: 201 });
+
+        var expiration = row["Vencimiento"].replace(/(\d+[/])(\d+[/])/, "$2$1");
+
+        await prisma.creditCard.upsert({
+          where: {
+            number: parseInt(row["Número"]),
+          },
+          create: {
+            number: parseInt(row["Número"]),
+            expiration:
+              "" === expiration ? new Date("01/01/1900") : new Date(expiration),
+            cvv: "" === row["CVV"] ? 999 : parseInt(row["CVV"]),
+            clientId: client.id,
+          },
+          update: {
+            expiration:
+              "" === expiration ? new Date("01/01/1900") : new Date(expiration),
+            cvv: "" === row["CVV"] ? 999 : parseInt(row["CVV"]),
+            clientId: client.id,
+          },
+        });
+      } catch (error) {
+        console.log(error);
+        return NextResponse.json("Error al importar datos", { status: 201 });
+      }
+      i++;
+    }
+  }
+
+  return NextResponse.json("Archivo subido correctamente", { status: 200 });
+}
