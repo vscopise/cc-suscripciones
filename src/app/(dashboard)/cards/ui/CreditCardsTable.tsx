2db9e1c475518204f35ad6d50cccf963bb7ca885
Vicente Scópise <vscopise@gmail.com>
2025-10-14 09:04:44 -0300

    14/10 - 1

--- src/app/(dashboard)/cards/ui/CreditCardsTable.tsx
+++ src/app/(dashboard)/cards/ui/CreditCardsTable.tsx
@@ -3,6 +3,7 @@
 import { CreditCard } from '@/interfaces';
 import { CreditCardsTableItem } from './CreditCardsTableItem';
 import Link from 'next/link';
+import { truncateString } from '@/utils';
 
 interface Props {
   creditCards: any[];
@@ -11,19 +12,25 @@ interface Props {
 export const CreditCardsTable = ({ creditCards }: Props) => {
   return (
     <table className="min-w-full">
-      <thead className="bg-gray-200 border-b">
+      <thead className="bg-gray-200 text-gray-900 border-b text-xs">
         <tr>
           <th
             scope="col"
-            className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
+            className="w-1/4 px-6 py-4 text-left"
           >
             Número
           </th>
           <th
             scope="col"
-            className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
+            className="w-1/4 px-6 py-4 text-left"
           >
             Cliente
+          </th>
+          <th
+            scope="col"
+            className="w-1/4 px-6 py-4 text-left"
+          >
+            Venc
           </th>
           <th />
         </tr>
@@ -31,16 +38,17 @@ export const CreditCardsTable = ({ creditCards }: Props) => {
       <tbody>
         {creditCards.map((card) => (
           <tr key={card.id} className="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100 text-gray-900">
-            <td className="text-sm  font-light px-6 py-4 whitespace-nowrap">
-              <Link href={`/card/${card.id}`} className='hover:underline px-6 py-4 block'>
+            <td className="text-sm  font-light px-6 py-4 whitespace-nowrap text-left">
+              <Link href={`/card/${card.id}`} className='hover:underline block'>
                 {card.number.toString()}
               </Link>
             </td>
             <td className="text-sm  font-light px-6 py-4 whitespace-nowrap">
-              <Link href={`/card/${card.id}`} className='hover:underline px-6 py-4 block'>
-                {`${card.client.name} ${card.client.lastName}`}
+              <Link href={`/card/${card.id}`} className='hover:underline block'>
+                {truncateString(`${card.client.name} ${card.client.lastName}`, 30)}
               </Link>
             </td>
+            <td className='text-sm  font-light px-6 py-4 whitespace-nowrap'>{card.expiration}</td>
             <td className="px-6 py-0 text-center">
               <div className="flex justify-center">
                 <CreditCardsTableItem creditCardId={card.id} />
